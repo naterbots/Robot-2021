b63e1e56507769519a456fd12c3cfcf612939602
GoldenGollem <20501643+GoldenGollem@users.noreply.github.com>
2021-03-18 22:53:48 -0500

    Created main framework

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.BarrelRace;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here.
+  public static Conveyor conveyor = new Conveyor();
+  public static Drivetrain drivetrain = new Drivetrain();
+  public static Intake intake = new Intake();
+  public static Shooter shooter = new Shooter();
+  public static Vision vision = new Vision();
+
+  // The joysticks are defined here.
+  public static Joystick opStick = new Joystick(Constants.OP_STICK_ID);
+  public static Joystick coopStick = new Joystick(Constants.COOP_STICK_ID);
+
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    //Do nothing for now
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return new BarrelRace();
+  }
+}
